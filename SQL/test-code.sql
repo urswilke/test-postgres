800efe0a93542a05ea17aa5f10d45e503640d3af
urswilke <urs.wilke@gmail.com>
2026-09-10 18:00:10 +0200

    create other tables

--- SQL/test-code.sql
+++ SQL/test-code.sql
@@ -11,3 +11,112 @@ CREATE TABLE public."Book" (
     "Parameter" text,
     "Productive" timestamp without time zone
 );
+
+
+ALTER TABLE public."Book" OWNER TO "TabBooks";
+
+
+CREATE TABLE public."Col" (
+    "BookNo" integer NOT NULL,
+    "ColNo" integer NOT NULL,
+    "HeadNo" integer NOT NULL,
+    "ColTitle1" text,
+    "ColTitle2" text,
+    "ColVariable" text,
+    "ColValue" double precision
+);
+
+
+ALTER TABLE public."Col" OWNER TO "TabBooks";
+
+
+CREATE TABLE public."Head" (
+    "BookNo" integer NOT NULL,
+    "HeadNo" integer NOT NULL,
+    "HeadName" text,
+    "HeadTitle" text,
+    "HeadCount" integer
+);
+
+
+ALTER TABLE public."Head" OWNER TO "TabBooks";
+
+
+CREATE TABLE public."Quest" (
+    "BookNo" integer NOT NULL,
+    "QuestNo" character varying(10) NOT NULL,
+    "QuestLine" integer NOT NULL,
+    "Title" character varying,
+    "Type" character varying NOT NULL,
+    "RowVar" character varying NOT NULL,
+    "Hash" character(32),
+    "Parameters" character varying,
+    "StartTime" timestamp without time zone,
+    "EndTime" timestamp without time zone,
+    "WarnLog" character varying,
+    "ErrorLog" character varying,
+    "CountRow" integer
+);
+
+
+ALTER TABLE public."Quest" OWNER TO "TabBooks";
+
+
+CREATE TABLE public."Row" (
+    "BookNo" integer NOT NULL,
+    "QuestNo" text NOT NULL,
+    "RowNo" integer NOT NULL,
+    "TabNo" integer NOT NULL,
+    "RowTypeS" text,
+    "RowType" integer,
+    "RowContent" text,
+    "RowContentDetail" text,
+    "RowAbsPercent" text,
+    "RowWeighted" text,
+    "RowTitle1" text,
+    "RowTitle2" text,
+    "RowTitle3" text,
+    "RowDecimals" integer,
+    "RowVariable" text,
+    "RowValue" double precision,
+    "RowFormat" integer,
+    "RowValues" double precision[]
+);
+
+
+ALTER TABLE public."Row" OWNER TO "TabBooks";
+
+
+CREATE TABLE public."Tab" (
+    "BookNo" integer NOT NULL,
+    "QuestNo" text NOT NULL,
+    "TabNo" integer NOT NULL,
+    "TabName" text,
+    "TabType" text,
+    "TabTitle" text,
+    "TabTitle1" text,
+    "TabTitle2" text,
+    "TabTitle3" text,
+    "TabRowTypes" integer,
+    "TabCaption" text,
+    "TabCount" integer,
+    "QuestLine" integer
+);
+
+
+ALTER TABLE public."Tab" OWNER TO "TabBooks";
+
+
+CREATE VIEW public."Val" AS
+ SELECT "Row"."BookNo",
+    "Row"."QuestNo",
+    "Row"."TabNo",
+    "Row"."RowNo",
+    ("Val"."ColNo" + 3) AS "ColNo",
+    "Val"."Value"
+   FROM public."Row",
+    LATERAL unnest("Row"."RowValues") WITH ORDINALITY "Val"("Value", "ColNo");
+
+
+ALTER VIEW public."Val" OWNER TO "TabBooks";
+
